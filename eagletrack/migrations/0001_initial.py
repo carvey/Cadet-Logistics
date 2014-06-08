56885c6226309d6102a76fbdfbbfe7df5286e500
charles <j95.charles@gmail.com>
2014-06-08 00:34:56 -0400

    Lots of changes to models. Adding of cadets and companies is now possible. Project is in a workable condition.

--- eagletrack/migrations/0001_initial.py
+++ eagletrack/migrations/0001_initial.py
@@ -12,6 +12,8 @@ class Migration(SchemaMigration):
         db.create_table('Company', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(default='alpha', max_length=2)),
+            ('co', self.gf('django.db.models.fields.related.OneToOneField')(db_index=False, related_name='company_cadet', unique=True, to=orm['eagletrack.Cadet'])),
+            ('platoons', self.gf('django.db.models.fields.related.ForeignKey')(related_name='company_platoons', db_index=False, to=orm['eagletrack.Platoon'])),
         ))
         db.send_create_signal(u'eagletrack', ['Company'])
 
@@ -20,9 +22,11 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('first_name', self.gf('django.db.models.fields.CharField')(max_length=25)),
             ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30)),
-            ('age', self.gf('django.db.models.fields.IntegerField')()),
-            ('company', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['eagletrack.Company'])),
+            ('age', self.gf('django.db.models.fields.PositiveIntegerField')()),
+            ('company', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['eagletrack.Company'], unique=True)),
             ('ms_level', self.gf('django.db.models.fields.CharField')(default='one', max_length=4)),
+            ('gpa', self.gf('django.db.models.fields.IntegerField')(default=4.0)),
+            ('ms_grade', self.gf('django.db.models.fields.IntegerField')(default=100)),
             ('is_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
             ('is_company_staff', self.gf('django.db.models.fields.BooleanField')(default=False)),
         ))
@@ -33,7 +37,7 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('first_name', self.gf('django.db.models.fields.CharField')(max_length=25)),
             ('last_name', self.gf('django.db.models.fields.CharField')(max_length=30)),
-            ('age', self.gf('django.db.models.fields.IntegerField')()),
+            ('age', self.gf('django.db.models.fields.PositiveIntegerField')()),
             ('rank', self.gf('django.db.models.fields.CharField')(max_length=25)),
             ('position', self.gf('django.db.models.fields.CharField')(max_length=75)),
         ))
@@ -41,7 +45,7 @@ class Migration(SchemaMigration):
 
         # Adding model 'Platoon'
         db.create_table(u'eagletrack_platoon', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            (u'company_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['eagletrack.Company'], unique=True, primary_key=True)),
         ))
         db.send_create_signal(u'eagletrack', ['Platoon'])
 
@@ -63,18 +67,20 @@ class Migration(SchemaMigration):
     models = {
         u'eagletrack.cadet': {
             'Meta': {'object_name': 'Cadet', 'db_table': "'Cadet'"},
-            'age': ('django.db.models.fields.IntegerField', [], {}),
-            'company': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['eagletrack.Company']"}),
+            'age': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'company': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['eagletrack.Company']", 'unique': 'True'}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
+            'gpa': ('django.db.models.fields.IntegerField', [], {'default': '4.0'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'is_company_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'ms_grade': ('django.db.models.fields.IntegerField', [], {'default': '100'}),
             'ms_level': ('django.db.models.fields.CharField', [], {'default': "'one'", 'max_length': '4'})
         },
         u'eagletrack.cadre': {
             'Meta': {'object_name': 'Cadre', 'db_table': "'Cadre'"},
-            'age': ('django.db.models.fields.IntegerField', [], {}),
+            'age': ('django.db.models.fields.PositiveIntegerField', [], {}),
             'first_name': ('django.db.models.fields.CharField', [], {'max_length': '25'}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
@@ -83,12 +89,14 @@ class Migration(SchemaMigration):
         },
         u'eagletrack.company': {
             'Meta': {'object_name': 'Company', 'db_table': "'Company'"},
+            'co': ('django.db.models.fields.related.OneToOneField', [], {'db_index': 'False', 'related_name': "'company_cadet'", 'unique': 'True', 'to': u"orm['eagletrack.Cadet']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'default': "'alpha'", 'max_length': '2'})
+            'name': ('django.db.models.fields.CharField', [], {'default': "'alpha'", 'max_length': '2'}),
+            'platoons': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'company_platoons'", 'db_index': 'False', 'to': u"orm['eagletrack.Platoon']"})
         },
         u'eagletrack.platoon': {
-            'Meta': {'object_name': 'Platoon'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+            'Meta': {'object_name': 'Platoon', '_ormbases': [u'eagletrack.Company']},
+            u'company_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['eagletrack.Company']", 'unique': 'True', 'primary_key': 'True'})
         }
     }
 
--- eagletrack/models.py
+++ eagletrack/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-'''Users is the base class for Cadet and Cadre'''
+"""Users is the base class for Cadet and Cadre"""
 class Users(models.Model):
     first_name = models.CharField(max_length = 25)
     last_name = models.CharField(max_length = 30)
@@ -13,31 +13,21 @@ class Users(models.Model):
     class Meta:
         abstract = True
         
+"""Company is the model for the companies in the batallion"""
 class Company(models.Model):
-    ALPHA = 'alpha'
-    BRAVO = 'bravo'
-    CHARLIE = 'charlie'
-    DELTA = 'delta'
-    
-    COMPANY_NAMES = (
-        (ALPHA, 'alpha'),
-        (BRAVO, 'bravo'),
-        (CHARLIE, 'charlie'),
-        (DELTA, 'delta')             
-        )
-    name = models.CharField(max_length = 2, choices = COMPANY_NAMES, default = ALPHA)
-    co = models.OneToOneField('Cadet', db_index=False, related_name='company_cadet', limit_choices_to={'is_company_staff':True})
-    platoons = models.ForeignKey('Platoon', db_index=False, related_name='company_platoons')
+    name = models.CharField(max_length = 10, default = "", help_text="Enter Name of the new company here")
+    commanding_officer = models.OneToOneField('Cadet', db_index=False, related_name='company_co', limit_choices_to={'is_company_staff':True}, blank=True, null=True, help_text="Enter the Name of the Commander Officer")
+    first_sergeant = models.OneToOneField('Cadet', db_index=False, related_name="company_firstsgt", limit_choices_to={'is_company_staff':True}, blank=True, null=True, help_text="Enter the First Sergeant for this Company")
+    platoons = models.ForeignKey('Platoon', db_index=False, related_name='company_platoons', blank=True, null=True)
     
     class Meta:
         db_table='Company'
     
     def __str__(self):
         return self.name
-    
+
+"""Cadet is the model for cadets in the batallion. This model extends the Users abstract model. Each cadet should ideally be assigned to a company"""
 class Cadet(Users):
-    def __init__(self):
-        pass
     one = 'one'
     two = 'two'
     three = 'three'
@@ -48,17 +38,18 @@ class Cadet(Users):
                 (three, 'MS3'),
                 (four, 'MS4'),
                 )
-    company = models.OneToOneField(Company)
+    company = models.OneToOneField(Company, blank=True, null=True)
     ms_level = models.CharField(max_length = 4,
                                 choices = ms_level_choices,
                                 default = one)
-    gpa = models.IntegerField(default=4.0)
+    gpa = models.DecimalField(default=4.0, max_digits=3, decimal_places=2)
     ms_grade = models.IntegerField(default=100)
     is_staff = models.BooleanField(default = False)
     is_company_staff = models.BooleanField(default = False)
     class Meta:
         db_table='Cadet'
-        
+
+"""The Cadre class is the model for cadre in the batallion"""
 class Cadre(Users):
     rank = models.CharField(max_length = 25)
     position = models.CharField(max_length = 75)

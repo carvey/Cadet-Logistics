74cbb1ae22b5a4e256686413c268c7a631931176
charles <j95.charles@gmail.com>
2014-06-19 20:02:49 -0400

    Added eagletrack views and urls. Also moved sbadmin and base templates out of eagletrack folder

--- eagletrack/urls.py
+++ eagletrack/urls.py
@@ -4,10 +4,15 @@ Created on Apr 27, 2014
 @author: carvey
 '''
 from django.conf.urls import patterns, url
-from eagletrack import views
+from eagletrack.views import index, CPview, Dashboard, CadetStats, CompanyStats, MSlevelStats
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),
+        url(r'^$', index, name='index'),
+        url(r'^cp', CPview.as_view(), name='cpview'),
+        url(r'^dash', Dashboard.as_view(), name='dashboard'),
+        url(r'^cadet', CadetStats.as_view(), name='cadetstats'),
+        url(r'^company', CompanyStats.as_view(), name='sompanystats'),
+        url(r'ms', MSlevelStats.as_view(), name='msstats'),
         
 )
 

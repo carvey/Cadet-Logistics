79db9b6f1138d709961020cbee7ccd16281d1d81
charles <charles@charles-desktop>
2014-06-21 02:23:55 -0400

    Eagletrack sidenavbar is set up & urls and views now reflect a standardized layout

--- eagletrack/urls.py
+++ eagletrack/urls.py
@@ -4,15 +4,18 @@ Created on Apr 27, 2014
 @author: carvey
 '''
 from django.conf.urls import patterns, url
-from eagletrack.views import index, CPview, Dashboard, CadetStats, CompanyStats, MSlevelStats
+from eagletrack.views import index, CPview, Dashboard, CadetStats, CadetListing, CompanyStats, CompanyListing, MSlevelStats, MSlevelListing
 
 urlpatterns = patterns('',
         url(r'^$', index.as_view(), name='index'),
         url(r'^cp', CPview.as_view(), name='cpview'),
         url(r'^dash', Dashboard.as_view(), name='dashboard'),
-        url(r'^cadet', CadetStats.as_view(), name='cadetstats'),
-        url(r'^company', CompanyStats.as_view(), name='sompanystats'),
-        url(r'ms', MSlevelStats.as_view(), name='msstats'),
+        url(r'^cadetstats', CadetStats.as_view(), name='cadetstats'),
+        url(r'^cadetlisting', CadetListing.as_view(), name='cadetlisting'),
+        url(r'^companystats', CompanyStats.as_view(), name='companystats'),
+        url(r'^companylisting', CompanyListing.as_view(), name='companylisting'),
+        url(r'^msstats$', MSlevelStats.as_view(), name='msstats'),
+        url(r'^mslisting', MSlevelListing.as_view(), name='mslisting'),
         
 )
 
--- eagletrack/views.py
+++ eagletrack/views.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django.views.generic import View
 
+from eagletrack.models import Cadet
+
 # Create your views here.
 
 class index(View):
-    template_name = "sbadmin/index.html"
+    template_name = "index.html"
     def get(self, request):
         return render(request, self.template_name, {})
+    
     
 class CPview(View):
     template_name = "eagletrack/control_panel.html"
@@ -19,24 +22,36 @@ class Dashboard(View):
     template_name = "eagletrack/dashboard.html"
     def get(self, request):
         return render (request, self.template_name, {})
-
-class CadetListing(View):
-    template_name = 'eagletrack/cadet_listing.html'
-    def get(self, request):
-        return render (request, self.template_name, {})
-
+    
 class CadetStats(View):
     template_name = 'eagletrack/cadet_stats.html'
     def get(self, request):
         return render (request, self.template_name, {})
 
+class CadetListing(View):
+    template_name='eagletrack/cadet_listing.html'
+    
+    cadets = Cadet.objects.all()
+    
+    def get(self, request):
+        return render (request, self.template_name, {'cadets': self.cadets})
+
 class CompanyStats(View):
     template_name = 'eagletrack/company_stats.html'
     def get(self, request):
         return render (request, self.template_name, {})
+    
+class CompanyListing(View):
+    template_name='eagletrack/company_listing.html'
+    def get(self, request):
+        return render(request, self.template_name, {})
 
 class MSlevelStats(View):
     template_name = 'eagletrack/ms_stats.html'
     def get(self, request):
         return render (request, self.template_name, {})
-    
+
+class MSlevelListing(View):
+    template_name='eagletrack/ms_listing.html'
+    def get(self, request):
+        return render (request, self.template_name, {})

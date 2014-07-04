e24d730ffd0e7e774b55c2457fb1bfd1305a4fa3
charles <j95.charles@gmail.com>
2014-07-04 17:07:56 -0400

    Fixed tabletools margin error and added additional fields and styling to the cadet profile page

--- personnel/templatetags/personnel_filters.py
+++ personnel/templatetags/personnel_filters.py
@@ -14,3 +14,28 @@ def default_blank(value):
         if value == None:
             return ' '
         
+@register.filter(name='active_cadet')  
+def active_cadet(dropped, commissioned=False):
+    if dropped == False:
+        if commissioned == False:
+            return "Active"
+    if dropped == True:
+        return "Dropped"
+    if commissioned == True:
+        return "Commissioned"
+    if dropped == True:
+        if commissioned == True:
+            return "Error: Dropped and Comissioned = True"
+    
+@register.filter(name="active_color")
+def active_color(dropped, commissioned=False):
+    if dropped == False:
+        if commissioned == False:
+            return "#00b300" #green
+    if dropped == True:
+        return "#FF0000" #red
+    if commissioned == True:
+        return "#000000" #black
+    if dropped == True:
+        if commissioned == True:
+            return "#FF0000" #red

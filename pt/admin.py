356a8be09e98b1f67739d4729ff1018f6174db5d
trent <trent@trent-Latitude-E6530>
2014-07-10 22:04:19 -0400

    Updated admin for pt to show Grader objects.

--- pt/admin.py
+++ pt/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from pt.models import PtTest, PtScore#, Time
+from pt.models import PtTest, PtScore, Grader
 
 
 class PtTestAdmin(admin.ModelAdmin):
@@ -10,3 +10,7 @@ admin.site.register(PtTest, PtTestAdmin)
 class PtScoreAdmin(admin.ModelAdmin):
     pass
 admin.site.register(PtScore, PtScoreAdmin)
+
+class GraderAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Grader, GraderAdmin)

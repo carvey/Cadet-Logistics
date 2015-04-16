fa43bd4305acb0925f34b22a96f7149b850a0f4c
charles <j95.charles@gmail.com>
2015-04-16 09:44:12 -0400

    fixed gpa

--- personnel/personnel_utils.py
+++ personnel/personnel_utils.py
@@ -79,10 +79,11 @@ def top_cumulative_scores(cadets):
     """
     cumalitive_score_dict = {}
     for cadet in cadets:
-        cadet_scores = PtScore.objects.filter(cadet=cadet)
-        score = PtScore.get_avg_total_score(cadet_scores) / float(100)
-        cumalitive = score + float(cadet.gpa)
-        cumalitive_score_dict[cumalitive] = cadet
+        if cadet.gpa:
+            cadet_scores = PtScore.objects.filter(cadet=cadet)
+            score = PtScore.get_avg_total_score(cadet_scores) / float(100)
+            cumalitive = score + float(cadet.gpa)
+            cumalitive_score_dict[cumalitive] = cadet
     return PtTest.order_scores_dict(cumalitive_score_dict, 5)
 
 # TODO docstring needs updating

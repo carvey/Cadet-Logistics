9932ef885ee409c08a7429cc0d4cc71f88c4d097
charles <j95.charles@gmail.com>
2015-08-09 17:10:15 -0400

    Fixed the last datatable bugs

--- templates/datatables/init_script.js
+++ templates/datatables/init_script.js
@@ -1,23 +1,34 @@
 {% load static %}
 
 //initilize the datatable with no settings so that all dom elements can be assigned
-var dt = $('.datatables-instance').dataTable({});
+var dt = $('.datatables-instance').dataTable({
+    destroy: true
+});
 
 //actual datatable initilization that will be used
 var final_dt = $('.datatables-instance').dataTable({
-    "destroy": true,
     "dom": 'TCR<"clear">lfrtip',
         "tableTools": {
             "sSwfPath": "{% static 'css/plugins/dataTables/plugins/TableTools/copy_csv_xls_pdf.swf' %}"
         },
 
         "fnInitComplete": function() {
-            $(".dataTables_filter").each(function() {
-                var exporting_container = $(this).siblings(".DTTT_container").get(0);
-                var vis_container = $(this).siblings(".ColVis").get(0);
+            //$(".dataTables_filter").each(function() {
+            //    var exporting_container = $(this).siblings(".DTTT_container").get(0);
+            //    var vis_container = $(this).siblings(".ColVis").get(0);
+            //
+            //    $(this).insertBefore(exporting_container);
+            //    $(vis_container).insertAfter(this);
+            //});
 
-                $(this).insertBefore(exporting_container);
-                $(vis_container).insertAfter(this);
+            $(".ColVis").each(function() {
+               var panel = $(this).parents('.panel').find('.right-panel-header').get(0);
+                $(this).appendTo(panel);
+            });
+
+            $(".DTTT_container").each(function() {
+               var panel = $(this).parents('.panel').find('.right-panel-header').get(0);
+                $(this).appendTo(panel);
             });
         },
         "bDestroy": true,

1e1bf900de159b53028c6043f9a37a183f0e6dc9
carvey <j95.charles@gmail.com>
2015-08-09 16:15:18 -0400

    Grouping pages now have an extra tab with an attendance datatable on it for exporting purposes. Datatables also now work with classes instead of IDs, so there can be multiple on a page. Still one problem left with cadet listing datatable. Want to push these changes before computer freezes again.

--- templates/datatables/init_script.js
+++ templates/datatables/init_script.js
@@ -1,40 +1,24 @@
 {% load static %}
 
 //initilize the datatable with no settings so that all dom elements can be assigned
-var dt = $('#datatables-instance').dataTable({});
-
-//setup search bars under all columns (before any are hidden)
-setup_column_search();
-
-//the method to handle adding the per column filtering
-function setup_column_search() {
-    $('#datatables-instance tfoot td').each( function () {
-        var title = $('#datatables-instance tfoot td').eq( $(this).index() ).text();
-        $(this).html( '<input type="text" class="form-control" style="width:100%" placeholder="Search '+title+'" />' );
-    });
-
-    var table = dt.api();
-
-    table.columns().eq( 0 ).each( function ( colIdx ) {
-        $( 'input', table.column( colIdx ).footer() ).on( 'keyup change', function () {
-            table
-                .column( colIdx )
-                .search( this.value )
-                .draw();
-        } );
-    } );
-}
+var dt = $('.datatables-instance').dataTable({});
 
 //actual datatable initilization that will be used
-var final_dt = $('#datatables-instance').dataTable({
+var final_dt = $('.datatables-instance').dataTable({
+    "destroy": true,
     "dom": 'TCR<"clear">lfrtip',
         "tableTools": {
             "sSwfPath": "{% static 'css/plugins/dataTables/plugins/TableTools/copy_csv_xls_pdf.swf' %}"
         },
 
         "fnInitComplete": function() {
-            $('.ColVis').appendTo(".right-panel-header");
-            $('.DTTT_container').appendTo('.right-panel-header');
+            $(".dataTables_filter").each(function() {
+                var exporting_container = $(this).siblings(".DTTT_container").get(0);
+                var vis_container = $(this).siblings(".ColVis").get(0);
+
+                $(this).insertBefore(exporting_container);
+                $(vis_container).insertAfter(this);
+            });
         },
         "bDestroy": true,
         "aoColumnDefs": [
